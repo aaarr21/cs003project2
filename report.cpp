fdf978ff415a6d6b94cbadc7720979efbe985aba
John <32312422+aaarr21@users.noreply.github.com>
2021-02-07 20:30:25 -0800

    Update report.cpp

--- report.cpp
+++ report.cpp
@@ -13,10 +13,21 @@
 #define GENDER(g) (g ? "Male" : "Female")
 
 int main() {
-  std::vector<Profile> up = profiles(10);
+    std::vector<Profile> up = profiles(100);
 
-  // Sample print for illustration, replace with your code
-  for (auto p : up) {
-    printf("%-8d %s\n", p.id, GENDER(p.gender));
-  }
+    std::map<uint32_t, Profile> profile_map;
+
+    for (auto p : up) {
+        profile_map[p.id] = p;
+    }
+
+    auto results = Match::pairs(up);
+
+    for (auto result : results) {
+        uint32_t id1 = result.first;
+        uint32_t id2 = result.second;
+        double score = get_total_score(profile_map[id1], profile_map[id2]);
+
+        printf("%-8d %-8d %lf\n", id1, id2, score);
+    }
 }
