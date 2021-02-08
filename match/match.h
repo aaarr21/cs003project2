6dfbe6f5434530eaf1833f334f36b228b3128634
John <32312422+aaarr21@users.noreply.github.com>
2021-02-07 20:29:29 -0800

    Update match.h

--- match/match.h
+++ match/match.h
@@ -13,8 +13,18 @@
 #include "data/data.h"
 
 class Match {
- public:
-  static std::map<uint32_t, uint32_t> pairs(std::vector<Profile> &);
+public:
+    static std::map<uint32_t, uint32_t> pairs(std::vector<Profile> & profiles);
 };
+
+
+double get_total_score(const Profile& profile1, const Profile& profile2);
+
+double country_score(const Profile& profile1, const Profile& profile2);
+double diet_score(const Profile& profile1, const Profile& profile2);
+double drinking_score(const Profile& profile1, const Profile& profile2);
+double language_score(const Profile& profile1, const Profile& profile2);
+double religion_score(const Profile& profile1, const Profile& profile2);
+double smoking_score(const Profile& profile1, const Profile& profile2);
 
 #endif  // MATCH_MATCH_H_
